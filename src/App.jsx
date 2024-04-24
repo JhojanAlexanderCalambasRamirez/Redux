1baa774e4df8637690c47eda4ffe2abe57f64e83
JhojanAlexanderCalambasRamirez <jhojan.calambas@uao.edu.co>
2024-04-23 19:09:58 -0500

    Explicacion Redux

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Counter } from "./feature/counter/";
+import { ShopingCar } from "./feature/shoping_car/";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <ShopingCar />
+    </div>
+  );
+}
+
+export default App;
